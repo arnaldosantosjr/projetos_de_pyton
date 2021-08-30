01902e49c3b14882d7c7c14fac1bcb126ee36f94
arnaldosantosjr <arnaldoleao6@gmail.com>
2021-08-30 19:33:08 -0300

    inserção de exercícios

---
+++ exercicios/ex002.py
@@ -0,0 +1,7 @@
+#Primeira forma
+nome = input('Olá! Me diga qual o seu nome.\n')
+print('Muito bem-vindo, ',nome,'!')
+
+#Segunda forma
+nome = input('Digite o seu nome outra vez.\n')
+print ('É um prazer te conhecer, {}!'.format(nome))
